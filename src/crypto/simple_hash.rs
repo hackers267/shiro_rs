9d6a3606b188020868f1ed3f36b02499c5a7f537
silence_zhpf <silence_zhpf@aliyun.com>
2023-08-09 22:49:23 +0800

    ✨ feat: 添加Sha1算法支持
    ✨ 添加Sha1算法支持

--- src/crypto/simple_hash.rs
+++ src/crypto/simple_hash.rs
@@ -1,11 +1,13 @@
 use base64ct::Base64;
 use base64ct::Encoding;
 use hex::encode;
+use sha1::Sha1;
 use sha2::Digest;
 use sha2::Sha256;
 
 pub enum Algorithm {
     SHA256,
+    SHA1,
 }
 
 pub struct SimpleHash {
@@ -22,12 +24,28 @@ impl SimpleHash {
                 let value = result.as_slice().to_vec();
                 Self { value }
             }
+            Algorithm::SHA1 => {
+                let mut hasher = Sha1::new();
+                hasher.update(source);
+                let result = hasher.finalize();
+                let value = result.as_slice().to_vec();
+                Self { value }
+            }
         }
     }
     pub fn with_salt(algorithm: Algorithm, source: &str, salt: &str) -> Self {
         match algorithm {
             Algorithm::SHA256 => {
                 let mut hasher = Sha256::new();
+                hasher.reset();
+                hasher.update(salt);
+                hasher.update(source);
+                let result = hasher.finalize();
+                let value = result.as_slice().to_vec();
+                Self { value }
+            }
+            Algorithm::SHA1 => {
+                let mut hasher = Sha1::new();
                 hasher.reset();
                 hasher.update(salt);
                 hasher.update(source);
@@ -49,6 +67,22 @@ impl SimpleHash {
                 let range = 1..times;
                 for _i in range {
                     let mut hasher = Sha256::new();
+                    hasher.reset();
+                    hasher.update(hashed);
+                    hashed = hasher.finalize();
+                }
+                let value = hashed.as_slice().to_vec();
+                Self { value }
+            }
+            Algorithm::SHA1 => {
+                let mut hasher = Sha1::new();
+                hasher.reset();
+                hasher.update(salt);
+                hasher.update(source);
+                let mut hashed = hasher.finalize();
+                let range = 1..times;
+                for _i in range {
+                    let mut hasher = Sha1::new();
                     hasher.reset();
                     hasher.update(hashed);
                     hashed = hasher.finalize();
@@ -80,20 +114,29 @@ mod tests {
     #[test]
     fn simple_hash_test() {
         let source = "admin";
-        let result = SimpleHash::simple(Algorithm::SHA256, source).to_hex();
+        let result = SimpleHash::simple(Algorithm::SHA256, source).to_string();
         assert_eq!(
             result,
             "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
         );
         let result = SimpleHash::simple(Algorithm::SHA256, source).to_base64();
-        assert_eq!(result, "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=")
+        assert_eq!(result, "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=");
+    }
+
+    #[test]
+    fn simple_hash_sha1_test() {
+        let source = "admin";
+        let result = SimpleHash::simple(Algorithm::SHA1, source).to_string();
+        assert_eq!(result, "d033e22ae348aeb5660fc2140aec35850c4da997");
+        let result = SimpleHash::simple(Algorithm::SHA1, source).to_base64();
+        assert_eq!(result, "0DPiKuNIrrVmD8IUCuw1hQxNqZc=");
     }
 
     #[test]
     fn simple_hash_salt_test() {
         let source = "admin";
         let salt = "123456";
-        let result = SimpleHash::with_salt(Algorithm::SHA256, source, salt).to_hex();
+        let result = SimpleHash::with_salt(Algorithm::SHA256, source, salt).to_string();
         assert_eq!(
             result,
             "2daceebc4e31654d326ae7889b397ed50ff7e5afff374d1f89525865fd87efe0"
@@ -103,28 +146,35 @@ mod tests {
     }
 
     #[test]
+    fn simple_hash_salt_sha1_test() {
+        let source = "admin";
+        let salt = "123456";
+        let result = SimpleHash::with_salt(Algorithm::SHA1, source, salt).to_string();
+        assert_eq!(result, "223ce7b851123353479d85757fbbf4e320d1e251");
+        let result = SimpleHash::with_salt(Algorithm::SHA1, source, salt).to_base64();
+        assert_eq!(result, "IjznuFESM1NHnYV1f7v04yDR4lE=");
+    }
+
+    #[test]
     fn simple_hash_salt_iter_test() {
         let source = "admin";
         let salt = "123456";
-        let result = SimpleHash::with_salt_iter(Algorithm::SHA256, source, salt, 10).to_hex();
+        let result = SimpleHash::with_salt_iter(Algorithm::SHA256, source, salt, 10).to_string();
         assert_eq!(
             result,
             "85c12dfa9332d0d096d78f1d93ea032a84489120d83a6b5d7ac4f968e65fc158"
         );
         let result = SimpleHash::with_salt_iter(Algorithm::SHA256, source, salt, 10).to_base64();
-        assert_eq!(result, "hcEt+pMy0NCW148dk+oDKoRIkSDYOmtdesT5aOZfwVg=")
+        assert_eq!(result, "hcEt+pMy0NCW148dk+oDKoRIkSDYOmtdesT5aOZfwVg=");
     }
 
     #[test]
-    fn simple_hash_salt_iter_test1() {
+    fn simple_hash_salt_iter_sha1_test() {
         let source = "admin";
-        let salt = "admin";
-        let result = SimpleHash::with_salt_iter(Algorithm::SHA256, source, salt, 16).to_string();
-        assert_eq!(
-            result,
-            "bf9e46e9b962c6b425f0685626cd64bb45febfac300c4b0322d0586954c20a26"
-        );
-        let result = SimpleHash::with_salt_iter(Algorithm::SHA256, source, salt, 16).to_base64();
-        assert_eq!(result, "v55G6blixrQl8GhWJs1ku0X+v6wwDEsDItBYaVTCCiY=")
+        let salt = "123456";
+        let result = SimpleHash::with_salt_iter(Algorithm::SHA1, source, salt, 10).to_string();
+        assert_eq!(result, "b8b8921827b021bdad0f5ded29c5b7404e1156d3");
+        let result = SimpleHash::with_salt_iter(Algorithm::SHA1, source, salt, 10).to_base64();
+        assert_eq!(result, "uLiSGCewIb2tD13tKcW3QE4RVtM=");
     }
 }
